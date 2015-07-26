2236a4fcdd4bbccc04c9e800ebffd0be7f417d72
Cody <codyn329@users.noreply.github.com>
2015-07-26 13:14:12 -0500

    Create Material.php

---
+++ Material.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * Material skin 
+ *
+ * @file
+ * @ingroup Skins
+ * @author Codyn329
+ * @version 1.0
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0
+ */
+ 
+ if( !defined('MEDIAWIKI') ) {
+        //return error message
+ }
+ 
+ //Skin credits for Special:Version
+ $wgExtensionCredits['skin'][] = array(
+        'path' => __FILE__,
+        'namemsg' => 'skinname-material',
+        'version' => '1.0',
+        'author' => '[https://meta.brickimedia.org/wiki/User:Codyn329 Codyn329]',
+        'descriptionmsg' => 'material-desc',
+        'license' => 'GPL-2.0+',
+ );
+ 
+ $wgValidSkinNames['material'] = 'Material';
+ $wgAutoloadClasses['skinMaterial'] = __DIR__ . '/Material.skin.php';
+ $wgMessageDirs['skinMaterial'] = __DIR__ . '/i18n';
+ 
+ $wgResourceModules['skins.material'] = array(
+        'styles' => array(
+                'skins/Material/material/main.css' => array( 'media' => 'screen' ),
+                'skins/Material/material/print.css' => array ('media' => 'print' ),
+        ),
+ );
