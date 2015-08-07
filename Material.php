6c236659e45ebac82794c29ada4881f403a7ae61
Jack Phoenix <jack@countervandalism.net>
2015-08-07 15:04:26 +0300

    Initial cleanup; no functional changes, just code style and whatnot

--- Material.php
+++ Material.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 /**
- * Material skin 
+ * Material skin
  *
  * @file
  * @ingroup Skins
@@ -8,29 +8,29 @@
  * @version 1.0
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0
  */
- 
- if( !defined('MEDIAWIKI') ) {
-        die('This skin requires installing MediaWiki. See https://www.mediawiki.org/wiki/Manual:Installation_guide for more information.');
- }
- 
- //Skin credits for Special:Version
- $wgExtensionCredits['skin'][] = array(
-        'path' => __FILE__,
-        'namemsg' => 'skinname-material',
-        'version' => '1.0',
-        'author' => '[https://meta.brickimedia.org/wiki/User:Codyn329 Codyn329]',
-        'descriptionmsg' => 'material-desc',
-        'license' => 'GPL-3.0',
- );
- 
- $wgValidSkinNames['material'] = 'Material';
- $wgAutoloadClasses['skinMaterial'] = __DIR__ . '/Material.skin.php';
- $wgMessageDirs['skinMaterial'] = __DIR__ . '/i18n';
- 
- $wgResourceModules['skins.material'] = array(
-        'scripts' => 'skins/Material/material/main.js',
-        'styles' => array(
-                'skins/Material/material/main.css' => array( 'media' => 'screen' ),
-                'skins/Material/material/print.css' => array( 'media' => 'print' ),
-        ),
- );
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die('This skin requires installing MediaWiki. See https://www.mediawiki.org/wiki/Manual:Installation_guide for more information.');
+}
+
+// Skin credits for Special:Version
+$wgExtensionCredits['skin'][] = array(
+	'path' => __FILE__,
+	'namemsg' => 'skinname-material',
+	'version' => '1.0',
+	'author' => '[https://meta.brickimedia.org/wiki/User:Codyn329 Codyn329]',
+	'descriptionmsg' => 'material-desc',
+	'license' => 'GPL-3.0',
+);
+
+$wgValidSkinNames['material'] = 'Material';
+$wgAutoloadClasses['SkinMaterial'] = __DIR__ . '/Material.skin.php';
+$wgMessageDirs['SkinMaterial'] = __DIR__ . '/i18n';
+
+$wgResourceModules['skins.material'] = array(
+	'scripts' => 'skins/Material/material/main.js',
+	'styles' => array(
+		'skins/Material/material/main.css' => array( 'media' => 'screen' ),
+		'skins/Material/material/print.css' => array( 'media' => 'print' ),
+	),
+);
--- Material.skin.php
+++ Material.skin.php
@@ -5,48 +5,51 @@
  * @file
  * @ingroup Skins
  */
- 
+
 // SkinTemplate class
 class SkinMaterial extends SkinTemplate {
-        public $skinname = 'material', 
-                $stylename = 'Material',
-                $template = 'MaterialTemplate', 
-                $useHeadElement = true;
-        //Add JS via ResourceLoader
-        public function initPage( OutputPage $out ) {
+	public $skinname = 'material',
+		$stylename = 'Material',
+		$template = 'MaterialTemplate',
+		$useHeadElement = true;
+
+	// Add JS via ResourceLoader
+	public function initPage( OutputPage $out ) {
 		parent::initPage( $out );
 		$out->addModules( 'skins.material.js' );
 	}
-        //Add CSS via ResourceLoader
-        function setupSkinUserCss( OutputPage $out ) {
+
+	// Add CSS via ResourceLoader
+	function setupSkinUserCss( OutputPage $out ) {
 		parent::setupSkinUserCss( $out );
 		$out->addModuleStyles( array(
-			'mediawiki.skinning.interface', 'skins.material'
+			'mediawiki.skinning.interface',
+			'skins.material'
 		) );
 	}
  }
- 
+
 // BaseTemplate class
 class MaterialTemplate extends BaseTemplate {
-        public function execute() {
+	public function execute() {
 		$this->html( 'headelement' );
 		?>
 		<header id="mw-header"> <!-- header -->
 			<form class="mw-search" id="searchform" action="<?php $this->text( 'wgScript' ); ?>"> <!-- search -->
 				<input type="hidden" name="title" value="<?php $this->text( 'searchtitle' ) ?>" />
 				<?php
-					echo $this->makeSearchInput( array( 'class' => 'mw-search-input' ) ); 
+					echo $this->makeSearchInput( array( 'class' => 'mw-search-input' ) );
 					echo $this->makeSearchButton( 'image', array( 'src' => $this->getSkin()->getSkinStylePath( 'images/magnifying-glass.svg'), 'alt' => 'search button' ) );
 				?>
 			</form>
 			<?php // language links
 				if ( $this->data['language_urls'] ) {
-					echo "<ul>";
+					echo '<ul>';
 					foreach ( $this->data['language_urls'] as $key => $langLink ) {
 						echo $this->makeListItem( $key, $langLink );
 					}
-					echo "</ul>";
-				} 
+					echo '</ul>';
+				}
 			?>
 		</header>
 		<div class="mw-body-content">
@@ -56,19 +59,19 @@ class MaterialTemplate extends BaseTemplate {
 				</div>
 			<?php } ?>
 			<?php if ( $this->data['newtalk'] ) { ?> <!-- new talk-->
-				<div class="new-talk"> 
-					<?php $this->html( 'newtalk' );?>
+				<div class="new-talk">
+					<?php $this->html( 'newtalk' ); ?>
 				</div>
 			<?php } ?>
 			<?php if ( $this->data['title'] != '' ) { ?> <!-- title section -->
 				<section id="title-section">
-					<?php //page status indicators
+					<?php // page status indicators
 						$oldVersion = version_compare( $wgVersion, '1.25', '<=' );
 						if ( $oldVersion ) {
-							if ( method_exists( $this, 'getIndicators') {
-								echo $this->getIndicators(); 
+							if ( method_exists( $this, 'getIndicators' ) {
+								echo $this->getIndicators();
 							}
-						} 
+						}
 					?>
 					<h1 class="first-heading"><?php $this->html( 'title' ); ?></h1> <!-- article heading -->
 					<div id="site-sub"><?php $this->msg( 'tagline' ); ?></div> <!-- tagline -->
@@ -79,11 +82,11 @@ class MaterialTemplate extends BaseTemplate {
 					<?php } ?>
 				</section>
 			<?php } ?>
-		        <?php $this->html( 'bodytext' ) ?> <!-- content -->
-		        <?php 
-				$this->html( 'catlinks' );  // categories 
+			<?php $this->html( 'bodytext' ) ?> <!-- content -->
+			<?php
+				$this->html( 'catlinks' );  // categories
 				if ( $this->data['dataAfterContent'] ) { // dataAfterContent
-					$this->html( 'dataAfterContent' );  
+					$this->html( 'dataAfterContent' );
 				}
 			?>
 		</div>
